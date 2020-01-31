d2a9c124fb9fb4cb32d1d8c3d3d93ceb450a0bb4
DenisChuvilin <denischuv@gmail.com>
2020-01-30 19:39:18 -0800

    finished and stretched

--- user_onboarding/src/components/MyForm.js
+++ user_onboarding/src/components/MyForm.js
@@ -4,13 +4,20 @@ import axios from 'axios';
 import * as yup from 'yup';
 import styled from 'styled-components';
 const P = styled.p`
-  color: green;
-`;
+    color: green;
+`
+const FormStyled = styled.form `
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+`
 
 const validationSchema = yup.object().shape({
+    
     name:yup
     .string().required("Type in your name!!!")
     .min(5,"NAME NOT LONG ENOUGH!!!!"),
+    
     password:yup
     .string().required("enter a passWOED PLEASE!!!")
     .min(6, "password in not long enough")
@@ -18,28 +25,49 @@ const validationSchema = yup.object().shape({
 })
 
 const MyForm = () => {
-  const handleSubmit = (values, tools) => {
-      tools.resetForm();
-    console.log(values);
+    const [users,setUsers] = useState([]);
+const handleSubmit = (values, tools) => {
+    tools.resetForm();
+    axios
+        .post("https://reqres.in/api/users",values)
+        .then(response => {setUsers([...users, response.data])})
+        .catch(error => console.log(error))
   };
-  return (
-    <Formik
-      initialValues={{ name: '', password: '', email: '' , checkbox: false }}
+return (
+    <div>
+        <Formik
+        initialValues={{ name: '', password: '', email: '' , checkbox: false, role:""}}
         validationSchema ={validationSchema}
-      onSubmit={handleSubmit}
-      render={props => (
-        <Form>
-          <Field type="text" name="name" placeholder="name" />
-          <P>{props.errors.name}</P>
-          <Field type="password" name="password" placeholder="password" />
-          <P>{props.errors.password}</P>
-          <Field type="email" name="email" placeholder="email" />
-          <P>{props.errors.email}</P>
-          <Field type="checkbox" name="checkbox" required />
-          <button type="submit">check box if thomas is awsome!</button>
-        </Form>
-      )}
-    />
+        onSubmit={handleSubmit}
+        render={props => (
+            <FormStyled>
+                <Field type="text" name="name" placeholder="name" />
+                    <P>{props.errors.name}</P>
+                <Field type="password" name="password" placeholder="password" />
+                    <P>{props.errors.password}</P>
+                <Field type="email" name="email" placeholder="email" />
+                    <P>{props.errors.email}</P>
+                <Field type="checkbox" name="checkbox" required />
+                <Field as="select" name="role"> 
+                    <option value=""></option>
+                    <option value="red">red</option>
+                    <option value="hare">hare</option>
+                    <option value="tree">tree</option>
+                    <option value="lake">lake</option>
+                </Field>
+                <button type="submit">check box if thomas is awsome!</button>
+            </FormStyled>
+        )}
+        />
+        <div>users:
+            {users.map(user => 
+                <div>
+                    <p><strong>{user.name}</strong></p>
+                    <p>{user.email}</p>
+                    <p>{user.role}</p>
+                </div>)}
+        </div>
+    </div>
   );
 };
 
