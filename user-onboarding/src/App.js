5f9feed26d6cbd10920a3ac8ea6c5d448e192417
DenisChuvilin <denischuv@gmail.com>
2020-03-06 22:17:52 -0800

    added follow along

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
-import { Form } from './components/Form';
+import FollowAlong from "./components/Follow-along"
+// import { Form } from './components/Form';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <Form />
+      {/* <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+      {/* </header> */}
+
+      <FollowAlong />
     </div>
   );
 }
--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,9 +1,9 @@
-import React from "react";
+// import React from "react";
 
-const Form = () => {
-    return (
-        null
-    )
-}
+// const Form = () => {
+//     return (
+//         null
+//     )
+// }
 
-export{Form};
+// export{Form};
